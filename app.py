22b5068268f2adb61b33114f702f948a2ea32684
RenBas <renbas9992000@gmail.com>
2026-07-11 12:13:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # ============================================================
 # app.py – entry point
 # ============================================================
+import sys
+print("Python version:", sys.version)
 from cdr_twin.ui import app
 
 if __name__ == "__main__":
